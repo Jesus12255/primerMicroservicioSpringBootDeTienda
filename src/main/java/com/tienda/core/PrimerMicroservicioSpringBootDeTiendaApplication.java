b7338b5f9063f16ce5bee91e727da66976456b40
admin <jh949742@gmail.com>
2024-01-20 21:55:26 -0500

    Creacion del proyecto en Spring Boot

---
+++ src/main/java/com/tienda/core/PrimerMicroservicioSpringBootDeTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerMicroservicioSpringBootDeTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerMicroservicioSpringBootDeTiendaApplication.class, args);
+	}
+
+}
